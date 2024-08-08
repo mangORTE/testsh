d03dce9e8a5b851ff642df6dab3db9fbf44888f3
Daniel Ortega <115849641+mangORTE@users.noreply.github.com>
2024-08-08 10:09:55 +0700

    Create install-cacti.sh

---
+++ install-cacti.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+apt-get update
+apt-get install -y apache2 rrdtool mariadb-server snmp snmpd php7.0 php-mysql php7.0-snmp php7.0-xml php7.0-mbstring php7.0-json php7.0-gd php7.0-gmp php7.0-zip php7.0-ldap php7.0-mc
