556ece87a18147e12238827f9475286c28f86d70
Daniel Ortega <115849641+mangORTE@users.noreply.github.com>
2024-08-08 10:31:13 +0700

    tes
    tes

--- install-cacti.sh
+++ install-cacti.sh
@@ -2,3 +2,12 @@
 
 apt-get update
 apt-get install -y apache2 rrdtool mariadb-server snmp snmpd php7.0 php-mysql php7.0-snmp php7.0-xml php7.0-mbstring php7.0-json php7.0-gd php7.0-gmp php7.0-zip php7.0-ldap php7.0-mc
+git clone -b 1.2.x  https://github.com/Cacti/cacti.git
+mv cacti /var/www/html
+# Database
+mysql -u root -p
+CREATE DATABASE cacti DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
+GRANT ALL PRIVILEGES ON cacti.* TO 'cacti'@'localhost' IDENTIFIED BY 'cacti';
+GRANT SELECT ON mysql.time_zone_name TO cacti@localhost;
+ALTER DATABASE cacti CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+FLUSH PRIVILEGES;
