c1266bca230703bb601be18045be04fbc6f5cae5
Daniel Ortega <115849641+mangORTE@users.noreply.github.com>
2024-08-08 15:55:33 +0700

    Update install-cacti.sh

--- install-cacti.sh
+++ install-cacti.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-apt-get update
-apt-get install -y apache2 rrdtool mariadb-server snmp snmpd php7.0 php-mysql php7.0-snmp php7.0-xml php7.0-mbstring php7.0-json php7.0-gd php7.0-gmp php7.0-zip php7.0-ldap php7.0-mc
-git clone -b 1.2.x  https://github.com/Cacti/cacti.git
-mv cacti /var/www/html
-# Database
-mysql -u root -p
-CREATE DATABASE cacti DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
-GRANT ALL PRIVILEGES ON cacti.* TO 'cacti'@'localhost' IDENTIFIED BY 'cacti';
-GRANT SELECT ON mysql.time_zone_name TO cacti@localhost;
-ALTER DATABASE cacti CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-FLUSH PRIVILEGES;
+apt update && apt upgrade
+# Install Apache for cacti
+apt install apache2
+systemctl enable --now apache2
+# Install PHP and MariaDB
+apt install php php-{mysql,curl,net-socket,gd,intl,pear,imap,memcache,pspell,tidy,xmlrpc,snmp,mbstring,gmp,json,xml,common,ldap} -y
+apt install libapache2-mod-php -y
+# Configure PHP memory and execution time
+
+
+
+
+
