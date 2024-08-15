0e2b04fadc87421d0ea6437c52018683a1060819
Daniel Ortega <115849641+mangORTE@users.noreply.github.com>
2024-08-16 00:20:27 +0700

    Update install-jenkins.sh

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -16,3 +16,41 @@ docker run \
   docker:dind \
   --storage-driver overlay2
   
+# Customize the official Jenkins Docker image
+cat <<EOF >Dockerfile
+FROM jenkins/jenkins:2.462.1-jdk17
+USER root
+RUN apt-get update && apt-get install -y lsb-release
+RUN curl -fsSLo /usr/share/keyrings/docker-archive-keyring.asc \\
+  https://download.docker.com/linux/debian/gpg
+RUN echo "deb [arch=$(dpkg --print-architecture) \\
+  signed-by=/usr/share/keyrings/docker-archive-keyring.asc] \\
+  https://download.docker.com/linux/debian \\
+  $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+RUN apt-get update && apt-get install -y docker-ce-cli
+USER jenkins
+RUN jenkins-plugin-cli --plugins "blueocean docker-workflow"
+EOF
+
+docker build -t myjenkins-blueocean:2.462.1-1 .
+
+docker run \
+  --name jenkins-blueocean \
+  --restart=on-failure \
+  --detach \
+  --network jenkins \
+  --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client \
+  --env DOCKER_TLS_VERIFY=1 \
+  --publish 8080:8080 \
+  --publish 50000:50000 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume jenkins-docker-certs:/certs/client:ro \
+  myjenkins-blueocean:2.462.1-1 
+
+  echo "######################## INSTALATION COMPLATE ####################################"
+  echo "Browse to http://localhost:8080"
+  
+  
+
+  
